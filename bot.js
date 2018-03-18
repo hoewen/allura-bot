7de585016711b164f5f4e7798f830b3bda7e11e8
hoewen <37493791+hoewen@users.noreply.github.com>
2018-03-18 01:08:00 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,11 +13,5 @@ client.on('message', message => {
       }
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
