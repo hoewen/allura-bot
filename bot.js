b7c69648a6a5891d3e71d753f7fde58cca23f498
hoewen <37493791+hoewen@users.noreply.github.com>
2018-03-18 13:07:25 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 client.on('message', message => {
      if(message.content.toLowerCase().includes("zarkon"))
                message.channel.send("We will defeat the Galra!")
-      }
+      })
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
